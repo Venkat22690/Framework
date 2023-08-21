2494d5743ec42c36ba71e12c7535ce29dc371875
unknown <venkataraman22690@gmail.com>
2023-08-21 20:22:22 +0530

    Updated

---
+++ src/test/java/RunnerPackage/TestMainClass.java
@@ -0,0 +1,35 @@
+package RunnerPackage;
+
+import java.io.IOException;
+
+import org.testng.annotations.Test;
+
+import Pages.APIMethods;
+@Test
+public class TestMainClass extends APIMethods{
+
+
+	public void MainMethod() throws IOException
+	{
+		// TODO Auto-generated method stub
+		
+			System.out.println("******* Test Execution Started *******");
+			
+		 	AccessToken();
+		 	AddNewCustomer();
+			ListCustomer();
+			NewPlans();
+		 	GetPlans();
+			NewMessage();
+			GetMessage();
+			GetPlanDocument();
+			GetOptimizations();
+			GetPlanQuestionnaire();
+			UpdatePlanResponse();
+			AddSubscription();
+			AssertionCalling();
+			
+		 	System.out.println("******* Test Execution Completed *******");
+		
+	}
+}
